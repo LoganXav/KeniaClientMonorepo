52e8635f5f67afb688d9a82bd758e27e8e725f1e
Segun <sogbesansegun22@gmail.com>
2024-06-07 22:25:03 +0100

    Fix: fixes

--- apps/web/layouts/base.tsx
+++ apps/web/layouts/base.tsx
@@ -1,20 +1,13 @@
 "use client"
-
 import type { FC, PropsWithChildren } from "react"
-
 import { NavigationStateProvider } from "@/providers/navigation-state-provider"
-import { cn } from "@repo/ui"
 
 const BaseLayout: FC<PropsWithChildren> = ({ children }) => (
-  <NavigationStateProvider>
-    <div
-      className={cn(
-        "min-h-screen scroll-smooth bg-background font-sans antialiased"
-      )}
-    >
-      {children}
-    </div>
-  </NavigationStateProvider>
+  // <NavigationStateProvider>
+  <div className="min-h-screen scroll-smooth bg-background font-sans antialiased">
+    {children}
+  </div>
+  // </NavigationStateProvider>
 )
 
 export default BaseLayout
